12e81ec474dd164c2074bbfc24b8205e80297805
k-hyeongyu <kampher@naver.com>
2025-05-01 14:13:15 +0900

    싱글톤 패턴

---
+++ src/cls04/Singleton.java
@@ -0,0 +1,10 @@
+package cls04;
+
+public class Singleton {
+	//싱글톤 : 전체에서 1개만 존재
+	
+	//외부의 생성 방지로 private사용
+	private Singleton() {
+		
+	}
+}
---
+++ src/cls04/Singleton_Main.java
@@ -0,0 +1,10 @@
+package cls04;
+
+public class Singleton_Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
